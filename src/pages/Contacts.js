4cd4930b59ca4b11bb4fc5e3aaa137004ff6d66a
maxxtron <maxxtron93@gmail.com>
2023-01-10 22:13:18 +0200

    fix

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -24,21 +24,15 @@ const Contacts = () => {
             <p>Kyiv, Ukraine</p>
           </li>
           <li className="content-list__item content-list__item--social">
-            <h2 className="title-2 title-2__flex">
-              <a href="tel:+380634463785">
-                <img src={phone} alt="phone" />
-              </a>
-            </h2>
-            <h2 className="title-2 title-2__flex">
-              <a href="https://t.me//maxxtron" target="_blank">
-                <img src={telegram} alt="telegram" />
-              </a>
-            </h2>
-            <h2 className="title-2 title-2__flex">
-              <a href="mailto:maxxtron93@gmail.com">
-                <img src={mail} alt="email" />
-              </a>
-            </h2>
+            <a href="tel:+380634463785">
+              <img src={phone} alt="phone" />
+            </a>
+            <a href="https://t.me//maxxtron" target="_blank">
+              <img src={telegram} alt="telegram" />
+            </a>
+            <a href="mailto:maxxtron93@gmail.com">
+              <img src={mail} alt="email" />
+            </a>
           </li>
         </ul>
       </div>
